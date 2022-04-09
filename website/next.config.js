50c57c349369fab01b4b9131699cb7f4a09b2ece
Denis Avakov <denis.avakov@pm.me>
2022-04-09 22:22:08 +0300

    chore: update defaults & configs

--- website/next.config.js
+++ website/next.config.js
@@ -1,6 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
+/**
+ * @type {import('next').NextConfig}
+ **/
+module.exports = {
+  reactStrictMode: true
+};
